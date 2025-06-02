5d107075c221fb64869d30f0e2b497c8a1a31ccd
Vincent Piard <vincent.piard44@gmail.com>
2025-06-02 15:53:33 +0200

    model ok

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :content
+  has_many :answers, dependent: :destroy
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :content_tags, dependent: :destroy
+  has_many :contents, through: :content_tags
+  validates :name, presence: true, uniqueness: true
 end
