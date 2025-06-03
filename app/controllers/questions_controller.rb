ff81f24971803ab8f9ce99e1ab5b935f68096cca
Vincent Piard <vincent.piard44@gmail.com>
2025-06-03 14:14:28 +0200

    questions ok

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,8 @@
 class QuestionsController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:new, :create]
-  
     def new
+    redirect_back(fallback_location: new_question_path)
     end
+
 
   def create
     transcription = params[:transcription]
