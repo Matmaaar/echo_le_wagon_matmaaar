3fc5d26492f674fdeb33f179e55d98189be77874
Vincent Piard <vincent.piard44@gmail.com>
2025-06-03 12:11:42 +0200

    test ok sur home

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:new, :create]
+  
+    def new
+    end
+
+  def create
+    transcription = params[:transcription]
+    questions = QuestionGeneratorService.new(transcription).call
+
+    flash[:questions] = questions
+    redirect_to new_question_path
+  end
 end
+
+
