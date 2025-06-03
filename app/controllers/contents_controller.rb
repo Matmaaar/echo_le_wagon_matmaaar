04fa63c6d8e03516239ebfd6768eb5fbe92e8f7c
augustin rouillard <augustin.rouillard@essca.eu>
2025-06-03 10:59:44 +0200

    Un utilisateur peux accéder la page d’un content, method controller done

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,4 +2,9 @@ class ContentsController < ApplicationController
   def index
     @contents = current_user.contents
   end
+
+  def show
+    @content = Content.find(params[:id])
+    @content.user = current_user
+  end
 end
