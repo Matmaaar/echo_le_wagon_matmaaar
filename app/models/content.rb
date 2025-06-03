072a7664947f3cb49d49bb81c5dc227502cfdacc
clement garreau <garreau.clem03@gmail.com>
2025-06-03 12:25:18 +0200

    en cours

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,4 @@
+   require "erb"
 class Content < ApplicationRecord
   belongs_to :user
   has_many :content_tags
@@ -5,4 +6,16 @@ class Content < ApplicationRecord
   has_many :questions
   has_many :notes
 
+
+  def get_transcript
+    api_url = "https://api.supadata.ai/v1/youtube/transcript?url=#{ERB::Util.url_encode(url)}&lang=fr"
+    response = HTTParty.get(
+      api_url,
+      headers: {
+        'x-api-key' => ENV['SUPADATA_API_KEY'],
+        'Content-Type' => 'application/json'
+      }
+    )
+    JSON.parse(response.body)
+  end
 end
