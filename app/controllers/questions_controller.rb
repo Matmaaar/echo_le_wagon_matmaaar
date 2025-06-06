32bd35772fb8f1be1183125a9b31c935a702cc1b
clement garreau <garreau.clem03@gmail.com>
2025-06-06 15:55:28 +0200

    identation ok

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,20 @@
 class QuestionsController < ApplicationController
-
- def new
-  @question = Question.new
-  @content = Content.find(params[:content_id]) if params[:content_id].present?
-end
-
-def create
-  @content = Content.find(params[:id])
-
-  questions_and_answers = QuestionGeneratorService.new(@content).call
-
-  if questions_and_answers.present?
-    flash[:notice] = "Question générée avec succès !"
-  else
-    flash[:alert] = "Erreur lors de la génération de la question."
+  def new
+    @question = Question.new
+    @content = Content.find(params[:content_id]) if params[:content_id].present?
   end
 
-  redirect_to content_path(@content)
-end
+  def create
+    @content = Content.find(params[:id])
 
+    questions_and_answers = QuestionGeneratorService.new(@content).call
+
+    if questions_and_answers.present?
+      flash[:notice] = "Question générée avec succès !"
+    else
+      flash[:alert] = "Erreur lors de la génération de la question."
+    end
+
+    redirect_to content_path(@content)
+  end
 end
