b5821890b75f7cb2771b6c070ae4b627ea0fe7f9
Matis Martin <matis.martin@outlook.fr>
2025-06-09 16:09:23 +0200

    refresh good

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,20 @@ class Message < ApplicationRecord
   validates :user_question, presence: true
   after_create :fetch_ai_answer
 
+  after_create_commit -> {
+    broadcast_append_to "messages_for_content_#{content.id}",
+                       target: "message",
+                       partial: "messages/message",
+                       locals: { message: self }
+  }
+
+   after_update_commit -> {
+    broadcast_replace_to "messages_for_content_#{content.id}",
+                        target: "message_#{id}",
+                        partial: "messages/message",
+                        locals: { message: self }
+  }
+
   private
 
   def fetch_ai_answer
