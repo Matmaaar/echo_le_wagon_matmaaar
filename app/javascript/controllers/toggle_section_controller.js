a2542c39ae6e1735af581c2a5ead1235712dcf71
augustin rouillard <augustin.rouillard@essca.eu>
2025-06-10 14:22:53 +0200

    le bouton edit à été modifié et changé de place, les classes actives sont setup sur les boutons de la show

--- app/javascript/controllers/toggle_section_controller.js
+++ app/javascript/controllers/toggle_section_controller.js
@@ -1,24 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["section"]
+  static targets = ["section", "button"]
 
   toggle(event) {
+    console.log(event.currentTarget)
     const targetId = event.currentTarget.dataset.targetId
     const targetSection = this.sectionTargets.find(section => section.id === targetId)
     if (!targetSection) return
 
-    // Si la section est déjà visible, ne rien faire
+    // Pour afficher ou non la section
     if (targetSection.style.display === "block") return
 
-    // Sinon, cacher toutes les sections
+    // On cache toutes les sections
     this.sectionTargets.forEach(section => {
       section.style.display = "none"
     })
 
-    // Afficher uniquement la section cible
+    // Afficher uniquement la bonne section
     targetSection.style.display = "block"
+
+    // Pour enlever les class
+    this.buttonTargets.forEach(btn => btn.classList.remove("btn-active"))
+    // Pour ajouter la classe css active (bouton blanc)
+
+    event.currentTarget.classList.add("btn-active")
   }
+
   hide(event) {
     const targetId = event.currentTarget.dataset.targetId
     const targetSection = this.sectionTargets.find(section => section.id === targetId)
