64030fa681fa84e0f24e660141a73106f5a9d9bd
Matis Martin <matis.martin@outlook.fr>
2025-06-30 19:52:22 +0200

    done

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,20 @@
 class Note < ApplicationRecord
   belongs_to :user
   belongs_to :content
+
+
+  def video_timestamp_in_seconds
+    return nil unless video_timestamp.present?
+    parts = video_timestamp.split(':').map(&:to_i)
+    case parts.length
+    when 3
+      hours, minutes, seconds = parts
+      hours * 3600 + minutes * 60 + seconds
+    when 2
+      minutes, seconds = parts
+      minutes * 60 + seconds
+    else
+      nil
+    end
+  end
 end
